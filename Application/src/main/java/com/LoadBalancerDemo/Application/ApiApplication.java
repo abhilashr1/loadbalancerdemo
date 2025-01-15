39077e9f59d22bef8600b6616a5d4c9cdf960d13
Abhilash <1995ad@gmail.com>
2025-01-16 01:15:09 +0700

    chore: initial project setup

---
+++ Application/src/main/java/com/LoadBalancerDemo/Application/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.LoadBalancerDemo.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ LoadBalancer/src/test/java/com/LoadBalancerDemo/LoadBalancer/LoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LoadBalancerDemo.LoadBalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b8ca956c06b2fc3fe2b2f2f42c94bd190361f64d
Abhilash <1995ad@gmail.com>
2025-01-17 02:16:04 +0700

    fix: try set default content type to fix chunk issue

--- LoadBalancer/src/main/java/com/LoadBalancerDemo/LoadBalancer/LoadBalancerApplication.java
+++ LoadBalancer/src/main/java/com/LoadBalancerDemo/LoadBalancer/LoadBalancerApplication.java
@@ -4,6 +4,8 @@ import io.netty.channel.ChannelOption;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -23,6 +25,7 @@ public class LoadBalancerApplication {
 						.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 3000)
 						.responseTimeout(Duration.ofSeconds(3))
 				))
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 				.build();
 	}
 
